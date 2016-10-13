4789aee93001f27299390bd94617662e72b4b59d
Sneha <snehashrotriya@utexas.edu>
2016-10-13 18:36:44 -0500

    views

--- app/src/main/java/com/ut/walkingbus/walkingbus/ParentAdapter.java
+++ app/src/main/java/com/ut/walkingbus/walkingbus/ParentAdapter.java
@@ -33,7 +33,7 @@ public class ParentAdapter extends RecyclerView.Adapter<ParentAdapter.MyViewHold
             status = (TextView) view.findViewById(R.id.status);
             chaperone_name = (TextView) view.findViewById(R.id.chaperone_status);
             call = view.findViewById(R.id.call);
-            message = view.findViewById(R.id.message_button);
+            message = view.findViewById(R.id.text);
             picture = (ImageView) view.findViewById(R.id.child_image);
         }
     }
