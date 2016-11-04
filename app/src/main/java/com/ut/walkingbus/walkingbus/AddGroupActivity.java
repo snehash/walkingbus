ae61a943c2fc57e64ed2ba17c960f21d8e868158
Ross McNulty <ross.mcnulty.1@gmail.com>
2016-11-04 05:14:36 -0500

    Update AddGroupActivity to avoid crashing (hopefully)

--- app/src/main/java/com/ut/walkingbus/walkingbus/AddGroupActivity.java
+++ app/src/main/java/com/ut/walkingbus/walkingbus/AddGroupActivity.java
@@ -1,21 +1,14 @@
 package com.ut.walkingbus.walkingbus;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class AddGroupActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener{
+public class AddGroupActivity extends AppCompatActivity {
     private static final String TAG = "AddGroupActivity";
 
     private String school;
@@ -33,79 +26,14 @@ public class AddGroupActivity extends AppCompatActivity
 
             @Override
             public void onClick(View arg0) {
+                String name = ((TextView) findViewById(R.id.add_name)).getText().toString();
+
                 Log.d(TAG, "adding group");
                 LoginActivity.getServerHelper().addGroup();
                 AddGroupActivity.super.onBackPressed();
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.actionbar_settings, menu);
-        return true;
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_parent) {
-            this.recreate();
-        } else if (id == R.id.nav_chaperone) {
-            Intent intent = new Intent(this, ChaperoneActivity.class);
-            startActivity(intent);
-        } else if(id == R.id.nav_sign_out) {
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
-        } else if (id == R.id.nav_group) {
-            Intent intent = new Intent(this, GroupActivity.class);
-            startActivity(intent);
-        } else if (id == R.id.nav_create_group) {
-            Intent intent = new Intent(this, AddGroupActivity.class);
-            startActivity(intent);
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 }
